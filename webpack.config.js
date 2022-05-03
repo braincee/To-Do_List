fdfa0a3082e4117762b582e0b13f56a9bb594632
braincee <kwesi938@gmail.com>
2022-05-03 11:03:43 +0100

    redo

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+const HtmlWebpackplugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    entry: path.resolve(__dirname, 'src/index.js'),
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'main.js',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                ],
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackplugin({
+            template: 'src/index.html',
+        }),
+    ],
+}
