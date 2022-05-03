2a9d6134ca77d17ac20fc3e35d90251aafc332ea
braincee <kwesi938@gmail.com>
2022-05-03 11:11:59 +0100

    redo

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,9 @@ const HtmlWebpackplugin = require('html-webpack-plugin');
 module.exports = {
     mode: 'development',
     entry: path.resolve(__dirname, 'src/index.js'),
+    devServer: {
+        static: './dist',
+    },
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'main.js',
